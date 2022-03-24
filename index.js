cce81e63d63b87f98a102a0f05da4020bf47724f
Albert Byrone <albertbyrone1677@gmail.com>
2022-03-24 15:02:06 +0300

    commit tree 21c71999dc71300583aeb8eb9089a309aee04125

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Your code here
+
+let  createEmployeeRecord =(row)=>{
+    return{
+        firstName : row[0],
+        familyName : row[1],
+        title: row[2],
+        payPerHour : row[3],
+        timeInEvents : [],
+        timeOutEvents : []
+    }  
+}
+
+let createEmployeeRecords = (employeesData) =>{
+    return employeesData.map(row => {
+        return createEmployeeRecord(row)
+    })
+}
+
+let createTimeInEvent = (record, time )=>{
+    let [date, hour] = time.split(' ')
+
+    record.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date,
+    })
+    return record
+}
+
+let createTimeOutEvent = (record, time) =>{
+    let [ date, hour ] = time.split(' ')
+    record.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date,
+    })
+    return record
+}
+
+let hoursWorkedOnDate =(record, date)=>{
+    let outEvent = record.timeOutEvents.find((e) =>{
+        return e.date === date
+    })
+
+    let inEvent = record.timeInEvents.find((e) =>{
+        return e.date === date
+    })
+    return (outEvent.hour - inEvent.hour )/100
+}
